b53c1305979d42981a037ba9919fd9f166ab4f36
Bradley Lauweres <Bradley.lauweres@student.howest.be>
2024-07-10 09:35:27 +0200

    Add Default chip data

---
+++ CHIP8Emulator/CHIP8Emulator/chip8.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <cstdint>
+
+class Chip8 {
+public:
+    Chip8();
+    void initialize();
+    void loadProgram(const char* filename);
+    void emulateCycle();
+    void setKeys();
+
+    bool drawFlag;
+
+private:
+    uint8_t memory[4096];
+    uint8_t V[16]; // CPU registers
+    uint16_t I; // Index register
+    uint16_t pc; // Program counter
+    uint8_t gfx[64 * 32]; // Graphics system
+    uint8_t delay_timer;
+    uint8_t sound_timer;
+    uint16_t stack[16];
+    uint16_t sp; // Stack pointer
+    uint8_t key[16]; // Hex-based keypad
+
+    // Opcodes
+    void decodeAndExecuteOpcode(uint16_t opcode);
+};
