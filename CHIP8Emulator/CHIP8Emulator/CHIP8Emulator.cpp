4eacddb141d386a4a99798e220bfb0b525a8a842
Bradley Lauweres <Bradley.lauweres@student.howest.be>
2024-07-10 16:21:37 +0200

    Add SDL to actually have graphics

--- CHIP8Emulator/CHIP8Emulator/CHIP8Emulator.cpp
+++ CHIP8Emulator/CHIP8Emulator/CHIP8Emulator.cpp
@@ -1,22 +1,85 @@
 #include <iostream>
 #include "chip8.h"
+#include <SDL.h>
 
-int main()
-{
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 320;
+
+int main(int argc, char* argv[]) {
     Chip8 chip8;
     chip8.initialize();
     chip8.loadProgram("../Games/SpaceInvaders.ch8");
 
-    // Emulation loop
-    for (;;) {
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    SDL_Window* window = SDL_CreateWindow("CHIP-8 Emulator",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        SCREEN_WIDTH, SCREEN_HEIGHT,
+        SDL_WINDOW_SHOWN);
+    if (!window) {
+        std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer) {
+        std::cerr << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Texture* texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888,
+        SDL_TEXTUREACCESS_STREAMING, 64, 32);
+
+    if (!texture) {
+        std::cerr << "SDL_CreateTexture Error: " << SDL_GetError() << std::endl;
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    bool running = true;
+    SDL_Event event;
+
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = false;
+            }
+            else if (event.type == SDL_KEYDOWN || event.type == SDL_KEYUP) {
+                // Handle key press/release events
+                // Map SDL keys to CHIP-8 keys
+            }
+        }
+
         chip8.emulateCycle();
 
         if (chip8.drawFlag) {
-        }
+            chip8.drawFlag = false;
 
-        chip8.setKeys();
+            uint32_t pixels[64 * 32];
+            for (int i = 0; i < 64 * 32; ++i) {
+                pixels[i] = (chip8.gfx[i] == 0) ? 0x000000FF : 0xFFFFFFFF;
+            }
+            SDL_UpdateTexture(texture, nullptr, pixels, 64 * sizeof(uint32_t));
+            SDL_RenderClear(renderer);
+            SDL_RenderCopy(renderer, texture, nullptr, nullptr);
+            SDL_RenderPresent(renderer);
+        }
+        SDL_Delay(16); // Roughly 60Hz
     }
+
+    SDL_DestroyTexture(texture);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
     return 0;
 }
-
